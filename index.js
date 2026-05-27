80da9766f8a22a79e9ac3f4b1d972a2b0112f0ee
Neurvoid <hsa1234567890123@gmail.com>
2026-05-27 18:14:41 +0530

    index.js

--- index.js
+++ index.js
@@ -16,3 +16,13 @@ bot.onText(/\/start/, (msg) => {
 });
 
 console.log("Bot started");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+res.send("Bot is running");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+console.log("Web server running");
+});
